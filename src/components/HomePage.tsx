f3061fabe622ff15f5483782d329ad52858387d4
Mohamed Mahmodi <mohammed5mmc@gmail.com>
2026-05-17 02:13:38 +0300

    hero section styling

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -15,21 +15,29 @@ export default function HomePage() {
   return (
     <ScreenLayout>
       <section id="home" className="h-screen">
+        <div className="absolute top-1/6 right-0 h-20 w-1/6 bg-primary/60 rounded-l-full"></div>
+        <div className="absolute top-1/4 translate-y-1/4 right-0 h-20 w-1/4 bg-primary/30 rounded-l-full"></div>
         <div className="relative w-full">
-          <h1 className={"text-8xl font-bold text-primary absolute top-20 left-[3%] " + (isAR ? "right-[5%] left-px" : "")}>
-            {t("firstname")}
-          </h1>
-          <h1 className={"text-8xl font-bold text-primary absolute top-60 left-[50%] " + (isAR ? "right-[50%] left-px" : "")}>
-            {t("lastname")}
-          </h1>
-          <div className={"absolute max-w-md left-[24%] -top-6 " + (isAR ? "right-[24%] left-px" : "")}>
-            <Image
-              className="h-125 w-auto"
-              src="/images/person-side.png"
-              alt={`${t("name")}'s picture`}
-              width={500}
-              height={600}
-            />
+          <div className={"flex flex-row justify-center"}>
+            <h1 className={"font-black text-primary mt-[clamp(1.5rem,5.5vw,5rem)] text-[clamp(1.5rem,5.5vw,4.8rem)]"}>
+              {t("firstname")}
+            </h1>
+            <div className="relative w-[clamp(10rem,30vw,100%)]">
+              <div className="absolute -left-1/3 bottom-0 h-1/4 w-full bg-primary/30 rounded-full -z-10"></div>
+              <div className="absolute -right-1/2 sm:-bottom-12 -bottom-8 h-1/4 w-[120%] bg-primary/50 rounded-full -z-10"></div>
+              {/* <div className="absolute -right-1/2 sm:-top-12 -left-8 h-[calc(50%+50px)] w-full bg-primary/50 rounded-full -z-10"></div> */}
+
+              <Image
+                className="shadow-[0_15px_20px_-28px_black] h-auto w-[clamp(10rem,30vw,100%)] max-h-125 rounded-br-[clamp(3rem,8vw,9rem)] self-start object-contain z-10"
+                src="/images/person-side.png"
+                alt={`${t("name")}'s picture`}
+                width={500}
+                height={600}
+              />
+            </div>
+            <h1 className={"font-black whitespace-nowrap text-primary self-end ms-[clamp(0rem,5vw,2rem)] mb-[clamp(1.5rem,5.5vw,5rem)] text-[clamp(2rem,5.5vw,4.8rem)]"}>
+              {t("lastname")}
+            </h1>
           </div>
         </div>
       </section>
